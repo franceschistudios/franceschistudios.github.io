f5ac27dca185a5c94cc0b0640abe5e03d25952e9
franceschistudios <47312171+franceschistudios@users.noreply.github.com>
2019-02-04 01:12:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-(function($){
-
-    function transform1() {
-        document.body.style.webkitTransform = 
-        document.body.style.MozTransform = 
-        document.body.style.OTransform = "rotate(180deg)";
-    }
-    
-    function transform2() {
-        var css = document.createElement('style');
-        css.type = 'text/css';
-        var styles = '*:hover{' +
-                '-moz-transition: all 1s ease;' +
-                '-moz-transition: rotate(180deg);' +
-                '-webkit-transition: all 1s ease;' +
-                '-webkit-transform: rotate(180deg);' +
-                '-o-transition: all 1s ease;' +
-                '-o-transition: rotate(180deg);' +
-            '}';
-        if (css.styleSheet) {
-            css.styleSheet.cssText = styles;
-        } else {
-            css.appendChild(document.createTextNode(styles));
-            document.getElementsByTagName('head')[0].appendChild(css);
-        }
-    }
-    
-    $('#one').click(function() {
-        transform1();
-
+$(document).ready(function() {
+    $("#menu li a").on('click', function() {
+        var page = $(this).data('page');
+        $("#pages .page:not('.hide')").stop().fadeOut('fast', function() {
+            $(this).addClass('hide');
+            $('#pages .page[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
+        });
     });
-    $('#two').click(function() {
-        transform2();
-
-    });
-
-})(jQuery);
+});
